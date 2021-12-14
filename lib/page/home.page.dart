edaea6e7fa3bd848f894ed40a59a72e018d47153
khaidem <khaidemdenish@gmail.com>
2021-12-14 13:50:42 +0530

    ok

--- lib/page/home.page.dart
+++ lib/page/home.page.dart
@@ -1,42 +1,16 @@
-import 'package:day8/logic/cubit/newsfed_cubit.dart';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import './newfeedModle.page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final state = context.watch<NewsfeedCubit>().state;
-    final status = state.status;
-
-    switch (status) {
-      case Status.initial:
-        return const Center(
-          child: CircularProgressIndicator(),
-        );
-      case Status.loading:
-        return const Center(
-          child: CircularProgressIndicator(),
-        );
-      case Status.loaded:
-        return Scaffold(
-          appBar: AppBar(
-            title: const Text('Home Screen'),
-          ),
-          body: Center(
-            child: TextButton(
-              child: const Text('Go to HomePageDetail'),
-              onPressed: () {
-                context.read<NewsfeedCubit>().getNewsFeedData();
-                // context.router.push(const HomeDetailRoute());
-              },
-            ),
-          ),
-        );
-      case Status.error:
-        return const Text("Error");
-    }
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('New Feed'),
+      ),
+      body: const NewsFeedModelPage(),
+    );
   }
 }
